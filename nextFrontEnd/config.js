3b2042364a72e5b9abcc69daa184368fad588349
Nikhil <nikhilyadav3000@gmail.com>
2020-04-10 00:11:26 +0530

    base url envt variable added

--- nextFrontEnd/config.js
+++ nextFrontEnd/config.js
@@ -1,4 +1,3 @@
-const config = {
-  baseURL: 'http://localhost:8080/test'
-}
+const baseURL = process.env.BASEURL
+const config = { baseURL: baseURL || 'http://localhost:8080/test' }
 module.exports = config
